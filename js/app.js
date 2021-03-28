7faa707b9a0f38ec3fbdf84cdfca496582ae2779
SuperHero?No <dmitrii.morozan@gmail.com>
2021-03-28 12:53:20 +0300

    added some pages.ready to verification

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
+// //===============Animation on scroll====================
+
+// const animItems = document.querySelectorAll('._anim-items');
+
+// if (animItems.length > 0) {
+// 	window.addEventListener('scroll', animOnScroll);
+
+// 	function animOnScroll() {
+// 		for (let index = 0; index < animItems.length; index++) {
+// 			const animItem = animItems[index];
+// 			const animItemHeight = animItem.offsetHeight;
+// 			const animItemOffset = offset(animItem).top;
+// 			const animStart = 4;
+
+// 			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+// 			if (animItemHeight > window.innerHeight) {
+// 				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+// 			}
+
+// 			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+// 				animItem.classList.add('_active'); //Попробовать сюда добавить вызов функции запуска счетчика
+
+// 			} else {
+// 				if (!animItem.classList.contains('_anim-no-hide')) {
+// 					animItem.classList.remove('_active');
+
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	function offset(el) {
+// 		const rect = el.getBoundingClientRect(),
+// 			scrollLeft = window.pageYOffset || document.documentElement.scrollLeft,
+// 			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// 		return {
+// 			top: rect.top + scrollTop,
+// 			left: rect.left + scrollLeft
+// 		};
+// 	}
+// 	animOnScroll();
+// }
+//=================================================
+
 function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
@@ -55,6 +99,10 @@ if (navLinks.length > 0) {
 		});
 	}
 }
+//========================================================================================================================================================
+
+
+
 
 "use strict";
 
