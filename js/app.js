ea2e24d6070158a970b581bd76faa8dcce7c33db
SuperHero?No <dmitrii.morozan@gmail.com>
2021-03-28 15:30:01 +0300

    added animation

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
 // //===============Animation on scroll====================
 
-// const animItems = document.querySelectorAll('._anim-items');
+const animItems = document.querySelectorAll('._anim-items');
 
-// if (animItems.length > 0) {
-// 	window.addEventListener('scroll', animOnScroll);
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
 
-// 	function animOnScroll() {
-// 		for (let index = 0; index < animItems.length; index++) {
-// 			const animItem = animItems[index];
-// 			const animItemHeight = animItem.offsetHeight;
-// 			const animItemOffset = offset(animItem).top;
-// 			const animStart = 4;
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
 
-// 			let animItemPoint = window.innerHeight - animItemHeight / animStart;
-// 			if (animItemHeight > window.innerHeight) {
-// 				animItemPoint = window.innerHeight - window.innerHeight / animStart;
-// 			}
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
 
-// 			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-// 				animItem.classList.add('_active'); //Попробовать сюда добавить вызов функции запуска счетчика
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active'); //Попробовать сюда добавить вызов функции запуска счетчика
 
-// 			} else {
-// 				if (!animItem.classList.contains('_anim-no-hide')) {
-// 					animItem.classList.remove('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
 
-// 				}
-// 			}
-// 		}
-// 	}
+				}
+			}
+		}
+	}
 
-// 	function offset(el) {
-// 		const rect = el.getBoundingClientRect(),
-// 			scrollLeft = window.pageYOffset || document.documentElement.scrollLeft,
-// 			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-// 		return {
-// 			top: rect.top + scrollTop,
-// 			left: rect.left + scrollLeft
-// 		};
-// 	}
-// 	animOnScroll();
-// }
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageYOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return {
+			top: rect.top + scrollTop,
+			left: rect.left + scrollLeft
+		};
+	}
+	animOnScroll();
+}
 //=================================================
 
 function testWebP(callback) {
@@ -99,8 +99,21 @@ if (navLinks.length > 0) {
 		});
 	}
 }
-//========================================================================================================================================================
+//==================Smooth Scroll========================
+const anchors = document.querySelectorAll('a[href*="#"]');
 
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+	});
+}
 
 
 
